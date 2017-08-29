ebe134c010e2ebf5d9a252fd6664ed3ff862cd35
Matt Andrews <matt@mattandrews.id.au>
2017-08-29 10:56:39 +1000

    add files from @aatteia

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# frontend-patterns
-HTML/CSS patterns for use in ALA sites.
+# ui-pattern-library
+
+UI / front-end (HTML/CSS) patterns for use in ALA sites.
+
+
+## Notes
+
+Adding ?debug=true to the end of URL will show a 8px baseline grid.
+
+
+
+
+Based on Barebones (<https://github.com/paulrobertlloyd/barebones>) by Paul Robert Lloyd (<http://paulrobertlloyd.com/>).
+
+
---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html><html lang="en-gb">
+
+<head>
+	<title>Barebones - An initial directory setup, style guide and pattern primer</title>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	<link rel="stylesheet" href="_css/styles.css" type="text/css"/>
+
+  <!-- Bootstrap -->
+  <link href="_css/bootstrap.min.css" rel="stylesheet">
+  <link href="_css/ala-pattern-library-styles.css" rel="stylesheet">
+
+  <!-- FontAwesome -->
+  <!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"> -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+	<!-- <header role="banner"> -->
+		<!-- <div class="container">
+			<h1 class="headline">Barebones</h1>
+			<p class="lede">An initial directory setup, style guide and pattern&#160;primer</p>
+		</div> -->
+
+    <div class="jumbotron info-hub-banner" id="ala-jumbotron example">
+      <div class="container">
+        <!-- <div class="col-lg-6 col-md-8 col-sm-12 promotional">The <strong>Atlas of Living Australia</strong> is a collaborative, national project that aggregates biodiversity data from multiple sources and makes it freely available and usable online</div> -->
+      </div>
+    </div>
+	<!-- </header> -->
+
+	<main role="main">
+    <div class="container">
+			<div class="row">
+        <article class="col-md-12 header-wrap margin-bottom-half-1">
+          <h5 class="subject-category-overline">Index</h5>
+          <h2 class="subject-title">ALA UI Pattern Library</h2>
+          <h3 class="subject-subtitle">The style guide and pattern&#160;primer for the Atlas of Living Australia. Patterns, text examples for applications and for articles and forms are included.</h3>
+          <!-- <p class="subject-byline">March 21, 2017</p> -->
+        </article>
+      </div>
+
+
+
+      <!-- <p>Intended as a starting point for my own projects, Barebones is freely available to fork and adapt for your own&#160;needs.</p>
+			<p><a class="button" href="https://github.com/paulrobertlloyd/barebones/archive/master.zip" download="barebones">Download Barebones</a> or&#160;<a href="https://github.com/paulrobertlloyd/barebones">view&#160;project&#160;on&#160;Github</a>.<p> -->
+			<h2>Contents</h2>
+			<dl>
+				<dt><a href="_styleguide.php">Style Guide</a></dt>
+				<dd>Basic markup and typographic styles for the application content type</dd>
+        <dt><a href="_article-styleguide.php">Article Style Guide</a></dt>
+        <dd>Typographic styles for the article content type</dd>
+				<dt><a href="_patterns.php">Pattern Library</a></dt>
+				<dd>Common snippets of markup</dd>
+				<dt><a href="_forms.php">Form Example</a></dt>
+				<dd>Basic form styles</dd>
+			</dl>
+		</div><!--/container-->
+	</main><!--@main-->
+
+	<footer role="contentinfo">
+		<div class="container">
+			<p><small>Copyright &#169; 2013 <a href="http://paulrobertlloyd.com">Paul Robert Lloyd</a>. Code covered by the <a rel="license" href="http://paulrobertlloyd.mit-license.org/">MIT license</a>.</small></p>
+		</div><!--/container-->
+	</footer><!--/@contentinfo-->
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="_js/bootstrap.min.js"></script>
+  <!-- <script src="_js/ala-wp-common.js"></script> -->
+</body>
+</html>
